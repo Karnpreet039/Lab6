ca79e127208a9b8a6ad0355b8487e90143b3c121
Karnpreet Grewal <karnpreetgrewal@Karnpreets-MacBook-Air.local>
2021-05-12 23:29:17 -0700

    structured- english

--- Lab_6.py
+++ Lab_6.py
@@ -2,7 +2,7 @@
 The start, we will generate a random interger between 1 and 20, and 
 baded on the result of the random number we check to see if it falls under certain range 
 if the number is greater than 15, then the result will be "Cherries"
-otherwiths if number is >18, then the result will be "Orange"
+otherwiths if number is >10, then the result will be "Orange"
 oterwise if number is > 5, then the result willl be "plum"
 othewiths if the numbes is >2, then the result will be "melon"
 otherwise if the number is > 1, then the result will be "bar"
@@ -10,4 +10,21 @@ otherwise if the number is > 1, then the result will be "bar"
 we iterate over using a loop three times and print the reults to the user. As an example "Plum cherries Melon"
 
 """
+"""
+import random 
+num = generate random number
 
+if num is greter thans 15, 
+    then the result will be ""
+Otherwise if num is greter thans 10, 
+    then the result will be ""
+Otherwise if num is greter thans 5, 
+    then the result will be ""
+Otherwise if num is greter thans 2, 
+    then the result will be ""
+Otherwise if num is greter thans 1, 
+    then the result will be ""
+
+loop three times
+    print the output (fret) to the user
+"""
